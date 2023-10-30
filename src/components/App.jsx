c3ce83ee92ee03241027e3d5c5fb71842e5c89ef
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2023-10-30 15:10:30 +0200

    statistic update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import data from '../assets/data.json';
 import friends from '../assets/friends.json';
 import items from '../assets/transactions.json';
 
-import { Profile } from './User profile/profile';
+import { Profile } from './UserProfile/profile';
 import { Statistics } from './Statistics/statistics';
 import { FriendList } from './FriendList/friendList';
 import { TransactionHistory } from './TransactionHistory/transactionHistory';
--- src/components/Statistics/statistics.jsx
+++ src/components/Statistics/statistics.jsx
@@ -1,4 +1,11 @@
-import { ItemLabel, ItemPercentage, StatisticsTitle, StatisticsWrapper, StatItem, StatList } from './statistics.styled'
+import {
+  ItemLabel,
+  ItemPercentage,
+  StatisticsTitle,
+  StatisticsWrapper,
+  StatItem,
+  StatList,
+} from './statistics.styled';
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
@@ -8,22 +15,18 @@ function getRandomHexColor() {
 
 export const Statistics = ({ title, stats }) => {
   return (
-  <StatisticsWrapper>
-  <StatisticsTitle>{title}</StatisticsTitle>
-
-  <StatList>
-      {stats.map(({ id, label, percentage }) => {
-        return (<StatItem key={id}
-          style={{ backgroundColor: getRandomHexColor() }}
-        >
-          <ItemLabel>{label}</ItemLabel>
-          <ItemPercentage>{percentage}%</ItemPercentage>
-        </StatItem>)
-      })
-      }
-  </StatList>
-</StatisticsWrapper>
-);
-}
-
-
+    <StatisticsWrapper>
+      {title && <StatisticsTitle>{title}</StatisticsTitle>}
+      <StatList>
+        {stats.map(({ id, label, percentage }) => {
+          return (
+            <StatItem key={id} style={{ backgroundColor: getRandomHexColor() }}>
+              <ItemLabel>{label}</ItemLabel>
+              <ItemPercentage>{percentage}%</ItemPercentage>
+            </StatItem>
+          );
+        })}
+      </StatList>
+    </StatisticsWrapper>
+  );
+};
